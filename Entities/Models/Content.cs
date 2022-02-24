0ce936b9f50b3e081c023f90f133dbec6edfeba6
Ravshan Sadikov <ravshan4ikov@gmail.com>
2022-02-24 21:05:50 +0500

    InitialCommit

---
+++ Entities/Models/Content.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Content
+    {
+        [Column("ContentId")]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string ShortDescription { get; set; }
+
+        [Required]
+        public string LongDescription { get; set; }
+
+        [Required]
+        public string ThumbnailUrl { get; set; }
+
+        public string GitUrl { get; set; }
+
+        public string YoutubeUrl  { get; set; }
+    }
+}
--- Repositories/AppDbContext.cs
+++ Repositories/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,7 +13,14 @@ namespace Repositories
     {
         public AppDbContext(DbContextOptions options) : base(options)
         {
+
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Content> Contents { get; set; }
     }
 }
